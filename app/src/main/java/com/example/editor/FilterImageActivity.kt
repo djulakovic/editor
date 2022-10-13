e31d5e84dfafcdae9585ded08d1195dad9a21a17
djulakovic <dusan_1998@hotmail.rs>
2022-10-13 11:49:12 +0200

    save image to gallery

--- app/src/main/java/com/example/editor/FilterImageActivity.kt
+++ app/src/main/java/com/example/editor/FilterImageActivity.kt
@@ -5,8 +5,16 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +22,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.zomato.photofilters.SampleFilters
 import com.zomato.photofilters.imageprocessors.Filter
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class FilterImageActivity : AppCompatActivity() {
@@ -33,7 +47,9 @@ class FilterImageActivity : AppCompatActivity() {
     private val permissionCode = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        supportActionBar?.hide()
+        supportActionBar?.title = "Filters"
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.DKGRAY))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_image)
         fetchViewChildren()
@@ -41,8 +57,100 @@ class FilterImageActivity : AppCompatActivity() {
         pickImage()
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permissionCode -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(this, R.string.permissions, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == imagePickCode && data != null) {
+            imageView.setImageURI(data.data)
+            fillImageFilters(data)
+            customAdapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_icons, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_save -> {
+                saveImage(imageView.drawable)
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun fillImageFilters(data: Intent?) {
+        data?.data?.let { FilterModel(it, R.string.originalFilter, Filter()) }?.let { itemsList.add(it) }
+        data?.data?.let { FilterModel(it, R.string.starLitFiler, SampleFilters.getStarLitFilter()) }?.let { itemsList.add(it) }
+        data?.data?.let { FilterModel(it, R.string.blueMessFilter, SampleFilters.getBlueMessFilter()) }?.let { itemsList.add(it) }
+        data?.data?.let { FilterModel(it, R.string.stuckVibeFilter, SampleFilters.getAweStruckVibeFilter()) }?.let { itemsList.add(it) }
+        data?.data?.let { FilterModel(it, R.string.limeStutterFilter, SampleFilters.getLimeStutterFilter()) }?.let { itemsList.add(it) }
+        data?.data?.let { FilterModel(it, R.string.nightWhisperFilter, SampleFilters.getNightWhisperFilter()) }?.let { itemsList.add(it) }
+    }
+
+    private fun getDisc(): File {
+        val file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        return File(file, "filters")
+    }
+
+    private fun saveImage(drawable: Drawable) {
+        val file = getDisc()
+
+        if (!file.exists() && !file.mkdirs()) {
+            file.mkdir()
+        }
+
+        val simpleDateFormat = SimpleDateFormat("yyyymmsshhmmss")
+        val date = simpleDateFormat.format(Date())
+        val name = "IMG$date.jpg"
+        val fileName = file.absolutePath + "/" + name
+        val newFile = File(fileName)
+
+        try {
+            val draw = drawable as BitmapDrawable
+            val bitmap = draw.bitmap
+            val fileOutPutStream = FileOutputStream(newFile)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutPutStream)
+            Toast.makeText(this, "File saved successfully", Toast.LENGTH_SHORT).show()
+            fileOutPutStream.flush()
+            fileOutPutStream.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
     private fun initRecycleView() {
-        customAdapter = CustomAdapter(itemsList,imageView)
+        customAdapter = CustomAdapter(itemsList, imageView)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = customAdapter
@@ -70,36 +178,5 @@ class FilterImageActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = intentType
         startActivityForResult(intent, imagePickCode)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            permissionCode -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    pickImageFromGallery()
-                } else {
-                    Toast.makeText(this, R.string.permissions, Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    private fun fillImageFilters(data: Intent?){
-        data?.data?.let { FilterModel(it, R.string.originalFilter, Filter()) }?.let { itemsList.add(it) }
-        data?.data?.let { FilterModel(it, R.string.starLitFiler, SampleFilters.getStarLitFilter()) }?.let { itemsList.add(it) }
-        data?.data?.let { FilterModel(it, R.string.blueMessFilter, SampleFilters.getBlueMessFilter()) }?.let { itemsList.add(it) }
-        data?.data?.let { FilterModel(it, R.string.stuckVibeFilter, SampleFilters.getAweStruckVibeFilter()) }?.let { itemsList.add(it) }
-        data?.data?.let { FilterModel(it, R.string.limeStutterFilter, SampleFilters.getLimeStutterFilter()) }?.let { itemsList.add(it) }
-        data?.data?.let { FilterModel(it, R.string.nightWhisperFilter, SampleFilters.getNightWhisperFilter()) }?.let { itemsList.add(it) }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == imagePickCode && data != null) {
-            imageView.setImageURI(data.data)
-            fillImageFilters(data)
-            customAdapter.notifyDataSetChanged()
-        }
     }
 }
